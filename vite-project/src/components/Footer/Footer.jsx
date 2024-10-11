f4f30bea05ed092fa98a09d495117d73082e5db8
Matt-Verni <matthewpmendez@gmail.com>
2024-10-11 19:56:21 -0400

    Initial Commit

---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import  '../../App';
+function Footer(){
+    return (
+        <div className="header-footer">Footer</div>
+    );
+}
+export default Footer;
---
+++ vite-project/src/components/Header/index.jsx
@@ -0,0 +1,8 @@
+
+import '../../App';
+function Header(){
+    return (
+        <div className="header-footer">Matthew Mendez</div>
+    );
+}
+export default Header;
