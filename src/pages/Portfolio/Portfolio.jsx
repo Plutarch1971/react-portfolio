d28101ed5f2f2e29980174ff231fa83be318163c
Matt-Verni <matthewpmendez@gmail.com>
2025-02-06 14:43:29 -0500

    Added Movie Tracer with Links

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -35,6 +35,9 @@ const Portfolio = () => {
                     }}
                     className="screenshot"
                     ></div>
+                    <div>
+                        <a href="https://github.com/Plutarch1971/my-personal-blog-post.git">My personal Blog Post Repo Link</a>
+                    </div>
                 </div>
 
                 <div className="box">
@@ -56,17 +59,26 @@ const Portfolio = () => {
                     }}
                     className="screenshot"
                     ></div>
+                    <div>
+                        <a href="https://github.com/JoeyVedder/4096.git">Puzzle Game 4096 Repository Link</a>
+                    </div>
                 </div>
 
                 <div className="box">
-                    <h3>Vehicle Update Commandline App</h3>
+                    <h3>Movie Tracker App</h3>
                     <div
                     style={{
-                        backgroundImage: "url('/Vehicle update application - commandline.png')"
+                        backgroundImage: "url('/movie-tracker-green.png')"
                     }}
                     className="screenshot"
-                    ></div>
+                    >
+                    </div>
+                 <div style={{display:'flex', justifyContent: 'space-between'}}>
+                 <a href="https://github.com/Plutarch1971/movie-tracker.git">Repository Link</a>
+                 <a href="https://movie-tracker-c1w4.onrender.com">Deployment Link</a>
                 </div>
+                </div>
+                
             </div>
         </div>
     );
